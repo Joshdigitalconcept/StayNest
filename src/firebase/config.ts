ca3b0713921c8b7a0b808f99269dd800552d1200
Fasinu Joshua <fasinujoshua0@gmail.com>
2025-12-19 09:34:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3087038929-64ef0",
+  "appId": "1:820595879272:web:87caa51369d0f68a2ebadd",
+  "apiKey": "AIzaSyCFHSWnnw1HbRBSVF7BxXyjy0OkCA1Bozk",
+  "authDomain": "studio-3087038929-64ef0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "820595879272"
+};
