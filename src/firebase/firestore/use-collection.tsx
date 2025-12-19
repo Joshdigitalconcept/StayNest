d55a5cd07d97a0a584a9054dd4acadee349dfb5f
Fasinu Joshua <fasinujoshua0@gmail.com>
2025-12-19 12:24:19 +0000

    Let's try this, I am using Next.js App Router with Firebase Firestore.

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -76,13 +76,10 @@ export function useCollection<T = any>(
     const unsubscribe = onSnapshot(
       memoizedTargetRefOrQuery,
       (snapshot: QuerySnapshot<DocumentData>) => {
-        const results: ResultItemType[] = snapshot.docs.map(doc => {
-            const docData = doc.data() as T;
-            return {
-                ...docData,
-                id: doc.id,
-            };
-        });
+        const results: ResultItemType[] = snapshot.docs.map(doc => ({
+          ...(doc.data() as T),
+          id: doc.id,
+        }));
         setData(results);
         setError(null);
         setIsLoading(false);
