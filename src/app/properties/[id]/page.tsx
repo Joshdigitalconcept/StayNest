636d21207c857801f7034c04bad9138678646429
Fasinu Joshua <fasinujoshua0@gmail.com>
2025-12-20 00:34:47 +0000

    Try fixing this error: `Runtime ReferenceError: useRouter is not defined

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from "next/image";
-import { notFound, useParams } from "next/navigation";
+import { notFound, useParams, useRouter } from "next/navigation";
 import * as React from 'react';
 import {
   Star,
