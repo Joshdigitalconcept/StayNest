1fa34b45b5a2f8da2296a13aba28f6060950bcc1
Fasinu Joshua <fasinujoshua0@gmail.com>
2026-01-08 16:36:16 +0000

    logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,12 +4,13 @@ import Image from 'next/image';
 export function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="https://i.ibb.co/zTzc0Sg/Whats-App-Image-2025-12-19-at-10-10-52-AM-1-1.png"
+      src="https://i.ibb.co/zTzc0Sg7/Whats-App-Image-2025-12-19-at-10-10-52-AM-1-1.png"
       alt="StayNest Logo"
       className={className}
       width={100}
       height={100}
       aria-hidden="true"
+      priority
     />
   );
 }
