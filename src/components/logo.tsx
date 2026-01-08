d4cb666a3871b9b7a505ca994db28925e97dd6df
Fasinu Joshua <fasinujoshua0@gmail.com>
2026-01-08 16:30:23 +0000

    nah, it's still StayNest, it's the brand logo that is showing broken ima

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="/logo.png"
+      src="https://i.ibb.co/zTzc0Sg/Whats-App-Image-2025-12-19-at-10-10-52-AM-1-1.png"
       alt="StayNest Logo"
       className={className}
       width={100}
